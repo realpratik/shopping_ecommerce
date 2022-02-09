8081abca318f74ac072f9cc69ad434b7b4fdff4c
Pratik Khadka <16739871+realpratik@users.noreply.github.com>
2022-02-10 08:28:48 +1000

    Update users.php

--- dashboard/users.php
+++ dashboard/users.php
@@ -27,7 +27,7 @@
 
         include "dashboardclass.php";
 
-        $obj = new DashboardClass("localhost","root","root","ecommerce");
+        $obj = new DashboardClass("yourhost","un","pw","db_name");
 
         $obj->show_users();
 
